e3c75386323a1dbaec353b38b8449c70aa9b4cb5
Socrates-baiano-Miguel <128157117+Socrates-baiano-Miguel@users.noreply.github.com>
2024-02-15 09:51:15 -0300

    Add files via upload

---
+++ jogogoa.py
@@ -0,0 +1,17 @@
+print ('*********************************')
+print ('Bem Vindo Ao Jogo De Adivinnhação')
+print ('*********************************')
+
+numeroS = 29
+
+chute = input('Determine Um Número')
+
+print('Você digitou o número', chute )
+
+if numeroS == chute: 
+    
+    print('Tu é Foda')
+
+else:
+    
+    print('Tu é Mt Ruim')
